4117e09a79142fe2b7cba46ca504335f10aa7a1b
skibidirizz67 <gitskibidirizz@proton.me>
2025-07-20 02:08:39 +0300

    logging, commands

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from telethon import TelegramClient, events
+import logging
+
+logging.basicConfig(format='[%(asctime)s] [%(levelname)s]: %(message)s', level=logging.INFO)
 
 f = open('api_id.txt', 'r')
 api_id = f.read()
@@ -6,18 +9,23 @@ f = open('api_hash.txt', 'r')
 api_hash = f.read()
 
 me = 5359995738
-target = 7808172033
+targets = [7808172033]
+
+cmds = {'тк':'ТКарточка', 'та':'ТАкк', 'мо':'Мои телефоны', 'мт':'Магазин телефонов', 'му':'Мои улучшения', 'ап':'Апгрейд', 'ен':'Ежедневная награда', 'са':'/sellall'}
 
 client = TelegramClient('anon', api_id, api_hash)
 
-@client.on(events.NewMessage(from_users=[target]))
+@client.on(events.NewMessage(from_users=targets))
 async def nm_handler(event):
     print(0)
 
 @client.on(events.NewMessage(outgoing=True, pattern=r'\!'))
 async def cmd_handler(event):
-    print(1)
+    text = event.text[1:]
+    for key in cmds:
+        if text == key:
+            await event.reply(cmds[key])
+            await event.delete()
 
 client.start()
 client.run_until_disconnected()
-client.disconnect()
