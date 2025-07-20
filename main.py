3d1705668929c002414bbb4d054bf91700a8835f
skibidirizz67 <gitskibidirizz@proton.me>
2025-07-20 03:52:29 +0300

    auto collection

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from telethon import TelegramClient, events
 import logging
+from threading import Timer
+import re
+from datetime import timedelta
+import asyncio
 
 logging.basicConfig(format='[%(asctime)s] [%(levelname)s]: %(message)s', level=logging.INFO)
 
@@ -9,23 +13,62 @@ f = open('api_hash.txt', 'r')
 api_hash = f.read()
 
 me = 5359995738
-targets = [7808172033]
+target = me
 
-cmds = {'тк':'ТКарточка', 'та':'ТАкк', 'мо':'Мои телефоны', 'мт':'Магазин телефонов', 'му':'Мои улучшения', 'ап':'Апгрейд', 'ен':'Ежедневная награда', 'са':'/sellall'}
+card_timer = Timer(0, None)
 
 client = TelegramClient('anon', api_id, api_hash)
 
-@client.on(events.NewMessage(from_users=targets))
-async def nm_handler(event):
-    print(0)
 
+cmds = {'тк':'ТКарточка', 'та':'ТАкк', 'мо':'Мои телефоны', 'мт':'Магазин телефонов', 'му':'Мои улучшения', 'ап':'Апгрейд', 'ен':'Ежедневная награда', 'са':'/sellall'}
 @client.on(events.NewMessage(outgoing=True, pattern=r'\!'))
 async def cmd_handler(event):
     text = event.text[1:]
     for key in cmds:
         if text == key:
-            await event.reply(cmds[key])
             await event.delete()
+            await event.reply(cmds[key])
+
+
+def txt_to_sec(text):
+    pattern = re.findall(r'(\d+)\s*(ч|м|с)', text, re.IGNORECASE)
+
+    h = m = s = 0
+    for v, u in pattern:
+        v = int(v)
+        u = u.lower()
+        if u in 'ч':
+            h += v
+        elif u in 'м':
+            m += v
+        elif u in 'с':
+            s += v
+    
+    return timedelta(hours=h, minutes=m, seconds=s).total_seconds()
+
+
+@client.on(events.NewMessage(from_users=target, pattern=r'(?s).*Вам выпал телефон!.*'))
+async def card_handler(event):
+    await event.reply(cmds['тк'])
+
+
+async def send_msg(target, text):
+    await client.send_message(target, text)
+def schedule_msg(loop, target, text):
+    asyncio.run_coroutine_threadsafe(send_msg(target, text), loop)
+
+
+@client.on(events.NewMessage(from_users=target, pattern=r'(?s).*Вы сможете выбить карту еще раз через.*'))
+async def cardt_handler(event):
+    global card_timer
+    card_timer.cancel()
+
+    time = txt_to_sec(event.text[event.text.rfind('з')+2:])
+    loop = asyncio.get_event_loop()
+
+    card_timer = Timer(time, lambda: schedule_msg(loop, target, cmds['тк']))
+    card_timer.start()
+
 
 client.start()
 client.run_until_disconnected()
