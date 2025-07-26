00ee621e80675c5f31b741d91c607c0a3a4c5fbc
skibidirizz67 <gitskibidirizz@proton.me>
2025-07-26 14:28:34 +0300

    minor code refactor

--- consts.py
+++ consts.py
@@ -1,27 +1,53 @@
-
-
 f = open('api_id.txt', 'r')
 api_id = f.read()
 f = open('api_hash.txt', 'r')
 api_hash = f.read()
 
 log_path = 'phoneget_userbot.log'
+log_format = '[%(asctime)s] [%(levelname)s]: %(message)s'
 
 target = '@phonegetcardsbot'
-
-cmds = {'тк':'ТКарточка', 'та':'ТАкк', 'мо':'Мои телефоны', 'мт':'Магазин телефонов', 'му':'Магазин улучшений', 'ап':'Апгрейд',
-'ен':'Ежедневная награда', 'са':'/sellall', 'тл':'Таблица лидеров', 'п':'/pay', 'ев':'/event', 'тр':'/trade',
-'км':'тк - ТКарточка\nта - ТАкк\nмо - Мои телефоны\nмт - Магазин телефонов\nму - Магазин улучшений\nап - Апргрейд\nен - Ежедневаня награда\nса - /sellall\nтл - Таблица лидеров\nп - /pay\nев - /event\nтр - /trade\nкм - Этот текст'}
-
 me = None
-async def getme():
-    global me
-    me = 'dikiy_opezdal' # TODO: client.get_me()
 
-patterns = {
-    'card_handler':rf'(?s)(?=.*@{me})(?=.*Вам выпал телефон!)',
-    'spam_handler':r'(?is).*розыгрыш.*|.*подписка на каналы.*',
-    'cardt_handler':rf'(?s)(?=.*@{me})(?=.*Вы сможете выбить карту еще раз через)',
-    'daily_handler':rf'(?s)(?=.*@{me})(?=.*Ежедневные награды:)',
-    'dailyt_handler':rf'(?s)(?=.*@{me})(?=.*Новая награда будет доступна завтра)',
+patterns = {}
+async def getme(client):
+    global me, patterns
+    me = await client.get_me()
+    me = me.username
+    patterns = {
+        'cmd_handler':r'\!',
+        'spam_handler':r'(?is).*розыгрыш.*|.*подписка на каналы.*',
+        'card_handler':rf'(?s)(?=.*@{me})(?=.*Вам выпал телефон!)',
+        'cardt_handler':rf'(?s)(?=.*@{me})(?=.*Вы сможете выбить карту еще раз через)',
+        'daily_handler':rf'(?s)(?=.*@{me})(?=.*Ежедневные награды:)',
+        'dailyt_handler':rf'(?s)(?=.*@{me})(?=.*Новая награда будет доступна завтра)'
+    }
+
+cmds = {
+    **dict.fromkeys(['тк', 'tc'], 'ТКарточка'),
+    **dict.fromkeys(['та', 'ta'], 'ТАкк'),
+    **dict.fromkeys(['мо', 'mp'], 'Мои телефоны'),
+    **dict.fromkeys(['мт', 'ps'], 'Магазин телефонов'),
+    **dict.fromkeys(['му', 'us'], 'Магазин улучшений'),
+    **dict.fromkeys(['ап', 'up'], 'Апгрейд'),
+    **dict.fromkeys(['ен', 'er'], 'Ежедневная награда'),
+    **dict.fromkeys(['са', 'sa'], '/sellall'),
+    **dict.fromkeys(['тл', 'lt'], 'Таблица лидеров'),
+    **dict.fromkeys(['п', 'p'], '/pay'),
+    **dict.fromkeys(['ев', 'ev'], '/event'),
+    **dict.fromkeys(['тр', 'tr'], '/trade'),
+    **dict.fromkeys(['км', 'h'],
+        '```тк|tc — ТКарточка\n'+
+        'та|ta — ТАкк\n'+
+        'мо|mp — Мои телефоны\n'+
+        'мт|ps — Магазин телефонов\n'+
+        'му|us — Магазин улучшений\n'+
+        'ап|up — Апгрейд\n'+
+        'ен|er — Ежедневная награда\n'+
+        'са|sa — /sellall\n'+
+        'тл|lt — Таблица лидеров\n'+
+        'п|p   — /pay\n'+
+        'ев|ev — /event\n'+
+        'тр|tr — /trade\n'+
+        'км|h  — This text\n```')
 }
--- main.py
+++ main.py
@@ -5,48 +5,48 @@ from datetime import timedelta
 import asyncio, signal, sys
 import consts as c
 
-logging.basicConfig(filename=c.log_path, filemode='a', format='[%(asctime)s] [%(levelname)s]: %(message)s', level=logging.INFO)
+
+logging.basicConfig(filename=c.log_path, filemode='a', format=c.log_format, level=logging.INFO)
 
 card_timer = Timer(0, None)
 daily_timer = Timer(0, None)
 
 client = TelegramClient('anon', c.api_id, c.api_hash)
 
-logging.info("\n\nSTARTING")
-client.start()
-logging.info("START complete")
-client.loop.run_until_complete(c.getme())
 
-@client.on(events.NewMessage(outgoing=True, pattern=r'\!'))
+logging.info('\n\nSTARTING')
+client.start()
+client.loop.run_until_complete(c.getme(client))
+logging.info('START complete')
+
+
+def txt_to_sec(text):
+    pattern = re.findall(r'(\d+)\s*(ч|м|с)', text, re.IGNORECASE)
+    h = m = s = 0
+    for v, u in pattern:
+        v = int(v)
+        u = u.lower()
+        if u in 'ч': h += v
+        elif u in 'м': m += v
+        elif u in 'с': s += v
+    return timedelta(hours=h, minutes=m, seconds=s).total_seconds()
+
+
+async def send_msg(target, text):
+    await client.send_message(target, text)
+    logging.info('[send_msg] sent "%s"', text)
+def schedule_msg(loop, target, text):
+    asyncio.run_coroutine_threadsafe(send_msg(target, text), loop) # TODO: remake, idk feels wrong
+
+
+@client.on(events.NewMessage(outgoing=True, pattern=c.patterns['cmd_handler']))
 async def cmd_handler(event):
     text = event.text[1:]
     for key in c.cmds:
         if text == key:
             await event.delete()
             await event.reply(c.cmds[key])
-
-
-def txt_to_sec(text):
-    pattern = re.findall(r'(\d+)\s*(ч|м|с)', text, re.IGNORECASE)
-
-    h = m = s = 0
-    for v, u in pattern:
-        v = int(v)
-        u = u.lower()
-        if u in 'ч':
-            h += v
-        elif u in 'м':
-            m += v
-        elif u in 'с':
-            s += v
-    
-    return timedelta(hours=h, minutes=m, seconds=s).total_seconds()
-
-
-@client.on(events.NewMessage(from_users=c.target, pattern=c.patterns['card_handler']))
-async def card_handler(event):
-    logging.info('[card_handler] triggered, sending cmd')
-    await event.reply(c.cmds['тк'])
+            return None
 
 
 @client.on(events.NewMessage(from_users=c.target, pattern=c.patterns['spam_handler']))
@@ -55,11 +55,10 @@ async def spam_handler(event):
     await event.delete()
 
 
-async def send_msg(target, text):
-    await client.send_message(target, text)
-    logging.info('[send_msg] sent "%s"', text)
-def schedule_msg(loop, target, text):
-    asyncio.run_coroutine_threadsafe(send_msg(target, text), loop)
+@client.on(events.NewMessage(from_users=c.target, pattern=c.patterns['card_handler']))
+async def card_handler(event):
+    logging.info('[card_handler] triggered, sending cmd')
+    await event.reply(c.cmds['tc'])
 
 
 @client.on(events.NewMessage(from_users=c.target, pattern=c.patterns['cardt_handler']))
@@ -98,22 +97,26 @@ async def dailyt_handler(event):
     logging.info('[dailyt_handler] waiting for %i seconds', time)
 
 
-
-def terminate(signum, frame):
-    card_timer.cancel()
-    daily_timer.cancel()
-    logging.info("STOP complete, terminating")
-    sys.exit(0)
-
 async def init():
-    logging.info("[init] setting timers")
+    logging.info('[init] setting timers')
     await client.send_message(c.target, c.cmds['тк'])
     await client.send_message(c.target, c.cmds['ен'])
 
 
-logging.info("INIT")
+def terminate(signum, frame):
+    logging.info('STOP')
+    card_timer.cancel()
+    daily_timer.cancel()
+    logging.info('STOP complete, terminating')
+    sys.exit(0)
+
+
+logging.info('INIT')
 signal.signal(signal.SIGTERM, terminate)
 client.loop.run_until_complete(init())
-logging.info("RUN")
+logging.info('INIT complete')
+
+logging.info('RUNNING')
 client.run_until_disconnected()
+
 terminate(0, 0)
